a685229f8fd1c0083643ef50d2ad0afee8eeace0
Haopeng Zeng <andyzhp234@gmail.com>
2022-10-31 05:37:06 -0700

    added Private Route Feature

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,16 +4,15 @@ import {
   getOrderItems,
   updateOrderItemPayment,
 } from '../controller/orderController.js'
+import { stripeCheckOut } from '../controller/stripeController.js'
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 
-// @desc    Fetch All products
-// @route   GET /api/products
-// @access  Public 
 router.post('/', protect, addOrderItems);
 router.get('/:id', protect, getOrderItems);
 router.put('/:id/pay', protect, updateOrderItemPayment);
+router.post('/create-checkout-session', protect, stripeCheckOut);
 
 export default router;
