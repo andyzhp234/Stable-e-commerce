7e5d61a75abcc17c307f78e36a02bcd977a3c1b4
Haopeng Zeng <andyzhp234@gmail.com>
2022-11-18 02:47:51 -0800

    fix(stripe): checkout issues

--- backend/routes/stripeRoutes.js
+++ backend/routes/stripeRoutes.js
@@ -1,10 +1,24 @@
-import express from 'express';
-import { stripeCheckOut, stripeWebHook } from '../controller/stripeController.js'
-import { protect } from '../middleware/authMiddleware.js';
+import express from "express";
+import {
+  stripeCheckOut,
+  stripeWebHook,
+} from "../controller/stripeController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import bodyParser from "body-parser";
 
 const router = express.Router();
 
-router.post('/create-checkout-session',express.json(), protect, stripeCheckOut);
-router.post('/webhook', express.raw({type: 'application/json'}), stripeWebHook);
+router.post(
+  "/create-checkout-session",
+  express.json(),
+  protect,
+  stripeCheckOut
+);
+
+router.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  stripeWebHook
+);
 
 export default router;
